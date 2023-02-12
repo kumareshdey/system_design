83b84e0278c596fca582ab016f46034621e98bda
Kumaresh Dey <kumaresh.dey7@gmail.com>
2023-02-12 22:13:51 +0530

    grp

--- splitwise/driver.py
+++ splitwise/driver.py
@@ -10,13 +10,13 @@ bill = BillService()
 user1 = user.addUser(id=1, name='kumaresh')
 user2 = user.addUser(id=2, name='sangita')
 
-group1 = group.addGroup(id='lovebirds',name='lovebirds', members=[user1,user2])
+group1 = group.addGroup(id='group1',name='group1', members=[user1,user2])
 
 paidBy = {1: 100}
 contribution = {1:50, 2: 50}
 
-billing = bill.addBill('billing1', 'lovebirds', contribution, paidBy )
-billing = bill.addBill('billing2', 'lovebirds', contribution, paidBy )
+billing = bill.addBill('billing1', 'group1', contribution, paidBy )
+billing = bill.addBill('billing2', 'group1', contribution, paidBy )
 
 print(bill.getUserBalance(1))
 print(bill.getUserBalance(2))
